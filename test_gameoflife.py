ba2f8a9b99ab8e614e1214bd65ff51cb410140a3
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-07 02:11:27 +0530

    Red:Implements function for get neighbor , test fais

--- test_gameoflife.py
+++ test_gameoflife.py
@@ -5,6 +5,14 @@ def test_generate_universe():
     cols = 3
     assert generate_universe(rows , cols) == [[0,0,0] , [0,0,0] , [0,0,0]]
     
+def test_get_neighbour():
+    grid = [[1,0,1] , 
+            [0,1,0] , 
+            [1,2,3]]
+    x_cordinate = 0
+    y_cordinate = 0
+    assert get_neighbour(grid , x_cordinate , y_cordinate) == [0,0,0,0,0,0,1]
+    
 
     
 
