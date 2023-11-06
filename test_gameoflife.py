217b0ecf213578110f75070314e377e59da14af4
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-07 01:54:12 +0530

    Red:Implements function for genarating grid, test fails

---
+++ test_gameoflife.py
@@ -0,0 +1,11 @@
+from gameoflife import *
+
+def test_generate_universe():
+    rows = 3
+    cols = 3
+    assert generate_universe(rows , cols) == [[0,0,0] , [0,0,0] , [0,0,0]]
+    
+
+    
+
+    
