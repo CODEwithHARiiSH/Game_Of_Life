c76d2ae6267afa4f472078034df6f01ff2fe5de1
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-08 14:37:35 +0530

    Makes output looks good.
    Deleted pygame file also.

--- run_gol.py
+++ run_gol.py
@@ -1,21 +1,23 @@
 from gameoflife import *
-
+import time
+import numpy as np
 def main():
-    rows = 10
-    cols = 10
-    grid=generate_universe(rows,cols,randomize=True)
-
-    print(grid)
-    display=show_display(grid)
-    print(display)
-    while True:
-        if all(cell == 0 for row in grid for cell in row):
-            break
-        grid = get_nextGeneration(grid)
+    rows = int(input("enter the rows--->"))
+    cols = int(input("enter the columns--->"))
+    grid=generate_random_universe(rows,cols,randomize = True)
+    zero_grid = generate_random_universe(rows,cols,randomize = False)
+    running = True
+    while running:
+        print('\033c')
         display = show_display(grid)
         print(display)
-
-
-
+        time.sleep(0.7)
+        next_grid = get_nextGeneration(grid)
+        if grid == zero_grid or grid == next_grid:
+            print('\033c')
+            display = show_display(grid)
+            print(display)
+            running = False
+        grid = next_grid
 if __name__ == '__main__':
     main()
