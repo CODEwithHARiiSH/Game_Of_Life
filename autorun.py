a61df4634aa8ad73747d32f2ae30dca57eeb9791
harish <harishthampyhat@gmail.com>
2023-11-08 18:21:42 +0530

    Made file easy to understand

--- autorun.py
+++ autorun.py
@@ -1,18 +1,17 @@
 from gameoflife import *
 import time
 import numpy as np
-
 def main():
     rows = 20
     cols = 20
+    grid=generate_universe(rows,cols,randomize = True)
     zero_grid = generate_universe(rows,cols,randomize = False)
-    grid = get_user_input(rows , cols)
     running = True
     while running:
         show_display(grid)
         time.sleep(0.7)
         next_grid = get_nextGeneration(grid)
-        if grid == zero_grid or grid == next_grid:
+        if grid == zero_grid:
             show_display(grid)
             running = False
         grid = next_grid
