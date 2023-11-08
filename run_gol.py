c15a58c353d7042e8285de24960d7ba95e378b5e
harish <harishthampyhat@gmail.com>
2023-11-08 18:16:21 +0530

    Fix bug

--- run_gol.py
+++ run_gol.py
@@ -8,15 +8,11 @@ def main():
     zero_grid = generate_universe(rows,cols,randomize = False)
     running = True
     while running:
-        print('\033c')
-        display = show_display(grid)
-        print(display)
+        show_display(grid)
         time.sleep(0.7)
         next_grid = get_nextGeneration(grid)
         if grid == zero_grid or grid == next_grid:
-            print('\033c')
-            display = show_display(grid)
-            print(display)
+            show_display(grid)
             running = False
         grid = next_grid
 if __name__ == '__main__':
